5a443520bf432c86bca18fc01894fba04ff6bba2
Pablo <pablo_082006@hotmail.com>
2021-03-02 21:53:07 -0600

    [Modified]: Server file indentation

--- Server.js
+++ Server.js
@@ -6,11 +6,11 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/json' });
     if (req.url.includes('/hello/')) {
         const name = req.url.split("/").pop()
-        res.end(JSON.stringify({hello : name}))
-    }else{
+        res.end(JSON.stringify({ hello: name }))
+    } else {
         res.statusCode = 404
         res.end(JSON.stringify({
-            statusCode : res.statusCode,
+            statusCode: res.statusCode,
             message: 'not found'
         }))
     }
